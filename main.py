5a86bb14d49744d705eacfb6f6a8ac1f85816bf3
muhammedSeyrek <muhammedseyrek00@gmail.com>
2024-08-23 16:36:14 +0300

    Second Program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient, events
 from telethon.tl.types import Channel
+import re
 
 apiId = input("Enter API ID: ").strip()
 apiHash = input("Enter API Hash: ").strip()
@@ -9,19 +10,50 @@ limit = input("Enter last message limit: ").strip()
 # Telegram istemcisini oluştur
 client = TelegramClient('session_name', apiId, apiHash)
 
-async def main():
-    #enter telegram
-    await client.start(phoneNumber)
-    print("Connect telegram!")
 
-    #last 10 message and list channels
+    
+async def main():
+    # Telegram'a giriş yap
+    await client.start(phoneNumber)
+    print("Connect Telegram!")
+    
+    # List subscribed channels
     async for dialog in client.iter_dialogs():
         if isinstance(dialog.entity, Channel):
-            print(f'\Channel Name: {dialog.name}, Channel ID: {dialog.id}')
-            messages = await client.get_messages(dialog.entity, limit = 10)
+            channel = dialog.entity
+            print(f'\n\n\nChannel Name: {channel.title}, Channel ID: {channel.id}')
+            
+            try:
+                # Take details on channel
+                full_chat = await client.get_entity(channel.id)
+                # Take description on channel
+                about = getattr(full_chat, 'about', 'No description available.')
+                print(f'Description: {about}')
+            except Exception as e:
+                print(f'An error occurred while getting the description: {e}')
+            
+            # Read messages up to the channel's last limit
+            messages = await client.get_messages(channel, int(limit))
             for message in messages:
-                print(f'Send: {message.sender_id}, Time: {message.date}, Message: {message.text}')
-        
+                # Take sender information
+                if message.sender_id:
+                    sender = await client.get_entity(message.sender_id)
+                    sender_name = sender.username or sender.first_name or "Unknown"
+                else:
+                    sender_name = "Unknown"
+                
+                # Check URL or Reference
+                if message.text:
+                    urls = re.findall(r'http[s]?://\S+', message.text)  # Regex using for URL
+                    if urls:
+                        print(f'Message: {message.text}')
+                        print(f'Found URLs: {urls}')
+                    else:
+                        print(f'Sender: {sender_name}, Time: {message.date}, Message: {message.text}')
+                else:
+                    print(f'Sender: {sender_name}, Time: {message.date}, Message: No text')
+
+
 # Run telegram client
 with client:
     client.loop.run_until_complete(main())
